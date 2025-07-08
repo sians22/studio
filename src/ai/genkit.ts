be1178addaf49c5b5a7f780023f82f187df073d4
Ugur Coskun <a70icin@gmail.com>
2025-07-08 20:56:20 +0000

    gemikey hatası veriyor

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,5 +2,9 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: process.env.GOOGLE_API_KEY,
+    }),
+  ],
 });
